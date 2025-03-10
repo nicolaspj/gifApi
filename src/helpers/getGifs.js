4577ed26d38a9b98aa44928f737f0ef7deeffb9c
Nicolas C <cardozo.j.nicolas@gmail.com>
2025-03-10 14:54:19 -0300

    cambios de api key

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,6 +1,6 @@
 export const getGifs = async (category) => {
 
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=jejejje=${category}&limit=10`
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=InsertarApiKey&q=${category}&limit=10`
     const resp = await fetch(url);
     const { data } = await resp.json();
 
