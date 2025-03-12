e8166a9fd1e88ae2679e07b0dcf4ef7b20d1bb93
Nicolas C <cardozo.j.nicolas@gmail.com>
2025-03-12 10:48:57 -0300

    add proptypes

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,6 +1,6 @@
 import { GiftItem } from './GiftItem';
 import  { useFetchGifs}   from '../hooks/useFetchGifs';
-
+import PropTypes from 'prop-types';
 export const GifGrid = ({ category }) => {
     const {images , isLoading} = useFetchGifs (category);
     
@@ -24,4 +24,8 @@ export const GifGrid = ({ category }) => {
         </div>
      </>
   )
+}
+
+GifGrid.prototype = {
+    category : PropTypes.string.isRequired,
 }
